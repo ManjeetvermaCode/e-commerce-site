b67be2d30be038dfa6244db03e10e19c7dcd9100
manjeetverma <manjeetverma@gmail.com>
2023-07-14 17:34:57 +0530

    added rating stars for displaying rating

--- frontend/frontend/src/product-card.jsx
+++ frontend/frontend/src/product-card.jsx
@@ -1,5 +1,7 @@
 import { Card } from "react-bootstrap"
 import { Link } from "react-router-dom"
+// as we are using client side rendering, while using 'a' tags will result in reloading of the page, therfore we'll use 'Link'.
+import Rating from "./rating"
 export default function Product({product}){
     return (
         <>
@@ -8,11 +10,16 @@ export default function Product({product}){
                     <Card.Img src={product.image} variant="top"/>
                 </Link>
                 <Card.Body>
-                    <Link to="{`/product/${product._id}`}">
-                        <Card.Title as="div">
-                            <strong>{product.name}</strong>
+                    <Link to="{`/product/${product._id}`"  >
+                        <Card.Title as="div" style={{overflow:'hidden',textOverflow:'ellipsis',whiteSpace:'nowrap',height:'2.5em'}} >//syle sets the tile in ellipsis format.
+                            <strong >{product.name}</strong>
                         </Card.Title>
                     </Link>
+
+                <Card.Text>
+                    <Rating value={product.rating} text={`${product.numReviews} Reviews` }/>
+                </Card.Text>
+
                 <Card.Text as='h3'>
                     ${product.price}
                 </Card.Text>
