a6caae5116ae302c4750077e7792c2cc72f967e4
manjeetverma <manjeetverma@gmail.com>
2023-07-17 20:28:15 +0530

    added feature that can fetch data from backend api endpoint for all products and a single product

---
+++ frontend/frontend/screens/home-screen.jsx
@@ -0,0 +1,44 @@
+import Product from '../src/components/product-card.jsx'
+import axios from 'axios'
+import { useEffect,useState } from 'react'
+
+import {Row,Col} from 'react-bootstrap'
+
+export default function HomeScreen(){
+    let [products,setProducts]=useState([])
+
+    useEffect(()=>{
+        const fetchProduct=async()=>{
+            try {
+                const response = await axios.get("http://localhost:3000/products");//we may want to replace the url, when uploading the site to the internet
+                
+                setProducts(response.data);  
+            } catch (error) {
+                console.error(error)
+            }
+            
+        }
+         fetchProduct()
+    },[])
+
+    
+    return (
+        <>
+        <h1>this is heaing</h1>
+        
+       
+        <Row>
+                {
+                    
+
+                   products.map(p=>
+                      <Col sm={12} md={6} lg={4} xl={2} >
+                         <Product product={p}/>
+                        </Col>
+                
+                    )
+                }
+            </Row> 
+        </>
+    )
+}
