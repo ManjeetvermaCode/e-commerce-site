e19fbcab01035221d9e76b67b171821af292e713
manjeetverma <manjeetverma@gmail.com>
2023-07-20 16:47:07 +0530

    imported mongoose and connected it with our cloud database i.e mongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,12 @@
 import dotenv from 'dotenv'
 dotenv.config()
+import connectDB from './config/db.js'
 import express from "express";//by setting "type":"module" we can use esModule syntax
+import products from './products.js'
 import cors from 'cors'
 const port=process.env.PORT || 5000
-
 const app=express()
-import products from './products.js'
-
+connectDB()//initializing the MONGODB instance.
 app.use(cors())
 
 app.get('/',(req,res)=>{
