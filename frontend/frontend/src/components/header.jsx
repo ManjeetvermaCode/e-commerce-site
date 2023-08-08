1e4512aae3d189c6094fadf3e628a61d51f3aa79
manjeetverma <manjeetverma@gmail.com>
2023-08-08 21:15:27 +0530

    added cart-screen and some corrections in the cartSlice

--- frontend/frontend/src/components/header.jsx
+++ frontend/frontend/src/components/header.jsx
@@ -5,13 +5,12 @@ import { useSelector } from 'react-redux';
 
 function Header() {
   const {cartItems}=useSelector((state)=>state.cart)
-  console.log(cartItems)
   return (
     <>
       <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
         <Container>
-          <LinkContainer to='/home'>
-            <Navbar.Brand>E-commerce site</Navbar.Brand>
+          <LinkContainer to='/'>
+            <Navbar.Brand >E-commerce site</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav'/>
           <Navbar.Collapse id='basic-navbar-nav'>
