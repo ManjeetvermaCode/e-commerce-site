49d40b92301f9b32ca87e27354876d6af8b643b9
manjeetverma <manjeetverma@gmail.com>
2023-08-18 17:53:40 +0530

    intergrated logout functionality from frontend to backend

--- frontend/frontend/src/constants.js
+++ frontend/frontend/src/constants.js
@@ -1,7 +1,9 @@
 
 export const BASE_URL=process.env.NODE_ENV==='development'?'http://localhost:5000':""
 export const PRODUCT_URL='/products'
-export const USER_URL='/user/login'
+export const USER_URL_LOGIN='/user/login'
+export const USER_URL_LOGOUT='/user/logout'
+
 export const ORDERS_URL='/orders'
 export const PAYPAL_URL='/config/paypal'
 
--- frontend/frontend/src/slices/loginSlice.js
+++ frontend/frontend/src/slices/loginSlice.js
@@ -1,20 +1,26 @@
-import { USER_URL } from "../constants";
+import { USER_URL_LOGIN,USER_URL_LOGOUT } from "../constants";
 import apiSlice from "./apiSlice";
 export const userApiSlice=apiSlice.injectEndpoints({//injecting endpoin into rootslice.
     endpoints:(builder)=>({
         login:builder.mutation({//used to make changes to data on the server (like creating or updating)
             query:(data)=>(
                 {
-                    url:USER_URL,
+                    url:USER_URL_LOGIN,
                     method:'post',
                     body:data
                     
                 }
             ),
         }),
+        logout:builder.mutation({
+            query:()=>({
+                url:USER_URL_LOGOUT,
+                method:'post'
+            })
+        })
          
     })
    
 })
 
-export const {useLoginMutation}=userApiSlice
+export const {useLoginMutation,useLogoutMutation}=userApiSlice
